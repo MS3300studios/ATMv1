42945c73335571f0adb23814be8f2fb845ccbe9f
Mikołaj Strusiński <mikistrus@gmail.com>
2020-03-03 22:33:28 +0100

    Created a Class
    Initial class has been made, with basic framework such as fields, data, constructors and ToString() that allows to interact with the class in the main program.cs

---
+++ ConsoleApp1/ATMLibrary/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMLibrary
+{
+    public class Class1
+    {
+        //fields and data
+        private double balance;
+        public double Balance
+        {
+            get { return balance; }
+            set
+            {
+                value = balance;
+            }
+        }
+        private string pin;
+        public string Pin
+        {
+            get { return pin; }
+            set
+            {
+                value = pin;
+            }
+        }
+
+
+        //Constructors
+        public Class1()
+        {
+            balance = 1000;
+            pin = "1234";
+        }
+        public Class1(double bal, string pinnr)
+        {
+            balance = bal;
+            pin = pinnr;
+        }
+
+
+        public override string ToString()
+        {
+            return $"account with balance: {balance} pin: {pin}";
+        }
+
+        //class methods
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATMLibrary;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 acc1 = new Class1(1000, "1234");
+            Console.WriteLine(acc1.ToString()); 
+        }
+    }
+}
